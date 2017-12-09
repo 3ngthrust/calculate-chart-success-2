52a08ca19ea5d7adc4e1f49d7307506a76289bc3
3ngthrust <marmon@posteo.de>
2017-12-09 13:20:52 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# chart_success.py
+A script to evaluate the chart success of an artist, writer or producer on a billboard chart over the years. 
+
+The simple metric calculated is as follows: A no. 1 song is valued as 100, a no. 2 song is valued as 99, etc., a no. 100 song is valued as 1. All song values of each chart of each year are added together.
+
+This is a improved version of [calculate-chart-success](https://github.com/3ngthrust/calculate-chart-success) with better accuracy. Here not only song titles are compared, but also the artist names. Therefore two lists are necessary instead of only one in the previous version.
+
+Usage
+-----
+1. Go to the discography of the person you want to evaluate on Wikipedia. Select the whole table with the songs (Ctrl+C). Copy the table to libre office (Ctrl+V). Make shure each song has its corresponding cell with the name of the artist. Split the artist cells horizontaly if necessary. Select the column with the song titles in libre office. Copy them (Ctrl+C) in libre and paste them into an an editor. Remove empty lines (Regex to find the lines: ^\n) and ". The result should be a list like the example 'Max_Martin_Songs_06-12-17' here. Do the same with the artist collum the result could look like 'Max_Martin_Artists_06-12-17'. Make shure every song is unique in the lists, duplicates are not removed.
+Hint use a shortcut in libre office to split the artist cells: Libre Office -> Tools -> Customize -> Keyboard-Tab -> Select Key Combinaton (e.g. F4) -> Select Table, Split Cells -> Click Modify -> Ok 
+
+2. Create a database of charts with the function "create_chart_database(chart_name, min_year, database_filename)". See the docstring of the function in chart_success.py for more information.
+
+3. Use the function calculate_success(database_filename, songlist_filename, artistlist_filename) to calculate the metric for each year. See the docstring of the function in chart_success.py for more information.
+
+3. Plot the results, a example is provided in visualisation.py
+
+Dependencies
+------------
+This script uses [billboard.py](https://github.com/guoguo12/billboard-charts) and [fuzzywuzzy](https://github.com/seatgeek/fuzzywuzzy).
+
+Example
+-------
+All the songs Max Martin contributed to (according to his Wikipedia discography as writer from 06.12.17) are listed in 'Max_Martin_Songs_06-12-17' and the corresponding artists in 'Max_Martin_Artists_06-12-17'. The result is shown below.
+
+![]()
+
+The added values for this example can be seen in 'Max_Martin_Output_06-12-17'. The output seems accurate. Of course errors are still possible since song titles and artist names can be similar and a fuzzy string comparison is used.
+
---
+++ chart_success.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: 3ngthrust
+"""
+from collections import OrderedDict
+from fuzzywuzzy import fuzz
+import billboard
+import time
+import pickle
+
+
+def song_value(songtitle, artist, chart):                                         
+# Run through one billboard chart objekt and return the "value" of the song 
+# with the specified Title and Artist in this chart.
+# The returned value for a no. 1 song is 100, the value of a no. 2 song is 99,
+# etc., the value of a no. 100 song is 1. If the song is not on the chart 0 is
+# returend.
+# Example: song_value("Song Title", "Artistname", chart)
+    
+    for chart_pos, song in enumerate(chart):                              
+        if (fuzz.token_set_ratio(songtitle, song.title) >= 90) and (fuzz.partial_ratio(artist, song.artist) >= 80):
+            return 100-chart_pos
+        
+    return 0
+   
+    
+def add_value_to_year(year, value, value_per_year):
+# Adds a value to a year in the dict value_per_year
+
+    if year in value_per_year:
+        value_per_year[year] = value_per_year[year] + value
+    else:
+        value_per_year[year] = value
+
+
+def create_chart_database(chart_name, min_year, database_filename):
+# Get all charts from the billboard chart with chart_name up to and including
+# the charts from min_year and save the database as database_filename.pkl
+# Example: create_chart_database('hot-100', 1993, 'hot-100-1993')
+
+    chart = billboard.ChartData(chart_name)
+    year = int(chart.date[:4])
+    chart_database = OrderedDict()
+     
+    while year >= min_year:     
+        # Output to give feedback on the progress
+        print(chart.date)        
+        print(year)
+        
+        # Include in Database
+        chart_database[chart.date] = chart
+
+        # Get older chart
+        older_chart = billboard.ChartData('hot-100', date=chart.previousDate)
+        while len(older_chart) != 100:
+            print('Could not download the chart, trying agian in 2 seconds.')
+            time.sleep(2)
+            older_chart = billboard.ChartData('hot-100', date=chart.previousDate)
+            
+        chart = older_chart
+        year = int(chart.date[:4])
+
+    # Save database
+    pickle.dump(chart_database , open(database_filename+".pkl", "wb"))
+    
+    
+def calculate_success(database_filename, songlist_filename, artistlist_filename):
+# Adds the song values of the songs in songlist_filename with the corresponding
+# artists in artistlist_filename in the charts in database_filename together
+# Example: calculate_success('database_filename,pkl', 'songlist_filename', 'artistlist_filename')
+    
+    # Import chart_database
+    chart_database = pickle.load(open(database_filename, "rb"))  
+    
+    # Import songlist and artistlist
+    songlist = open(songlist_filename).read().split('\n')
+    artistlist = open(artistlist_filename).read().split('\n')
+                  
+    value_per_year = dict()
+    
+    for chart_date, chart in chart_database.items():
+        
+        print(chart_date + ":" + "\n")
+        
+        year = int(chart_date[:4])
+        
+        for i, song in enumerate(songlist):   
+            value = song_value(song, artistlist[i], chart)
+        
+            if value:
+                add_value_to_year(year, value, value_per_year)
+                print(song + ' by ' + artistlist[i] + ' ' + str(value) + "\n")
+                
+    print(value_per_year)
+    return value_per_year
+    
+        
+if __name__ == "__main__":
+    
+    # create_chart_database('hot-100', 1993, 'hot-100-1993')
+    calculate_success('hot-100-1993.pkl', 'Max_Martin_Songs_06-12-17' , 'Max_Martin_Artists_06-12-17')
